2be63ea2bedaffc89b1eb58654566050a718e940
Matheus Castro <matheus.castro@ciag.org.br>
2026-08-17 16:06:29 -0300

    feat: expose PrimeNG-compatible root class names

--- projects/orc-ds/public-api.ts
+++ projects/orc-ds/public-api.ts
@@ -155,3 +155,4 @@ export * from '@ciag/orchestra/datepicker';
 export * from '@ciag/orchestra/compare';
 export * from '@ciag/orchestra/input-otp';
 export * from '@ciag/orchestra/radio-button';
+export * from './primeng-api';
